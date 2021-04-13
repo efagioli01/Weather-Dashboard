2b01a846ed80bc875f63e964de06568216b46a31
Erica Fagioli <efagioli01@gmail.com>
2021-04-13 12:28:34 -0400

    added uvi to page, rounded temp, wind, uvi, humidity numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    const apiKey = "3c93e73d72c848cc06d03b3131ddc61f";
+    let apiKey = "3c93e73d72c848cc06d03b3131ddc61f";
 
     let date = new Date();
 
@@ -17,80 +17,133 @@ $(document).ready(function () {
         if (city) {
             searchWeather(city);
         }
-        
-    })
+
+    });
+
+    // var history = JSON.parse(localStorage.getItem('"history')) || [];
+
+    // if (history.length > 0) {
+    //     weatherFunction(history[historylength -1]);
+    // }
+
+    // for (var i = 0; i < history.length; i++) {
+    //     createRow(history[1]);
+    // }
+
+    // function createRow(text) {
+    //     var listItem =$("<li>").addClass("list-group-item").text(text);
+    //     $(".history").append(listItem);
+    // }
+
+    // $(".history").on("click", "li", function() {
+    //     weatherFunction($(this).text());
+    //     weatherForecast($(this).text());
+
+    // });
+    // function weatherFunction(searchTerm) {
+    // }
+
+    
 
     function searchWeather(city) {
 
-        let endPoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        let endPoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
         fetch(endPoint)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
 
-            let todayEl = document.getElementById("today")
-            todayEl.textContent = ""
+                let todayEl = document.getElementById("today")
+                todayEl.textContent = ""
 
-            let titleEl = document.createElement('h3')
-            titleEl.classList.add('card-title')
-            titleEl.textContent = `${data.name} (${new Date().toLocaleDateString()})`
+                let titleEl = document.createElement('h3')
+                titleEl.classList.add('card-title')
+                titleEl.textContent = `${data.name} (${new Date().toLocaleDateString()})`
 
-           let cardEl = document.createElement('div') 
-           cardEl.classList.add('card')
+                let cardEl = document.createElement('div')
+                cardEl.classList.add('card')
 
-           let cardBodyEl = document.createElement('div')
-           cardBodyEl.classList.add('card-body')
+                let cardBodyEl = document.createElement('div')
+                cardBodyEl.classList.add('card-body')
 
-           let windEl = document.createElement('p')
-           windEl.classList.add('card-text')
-           
-           let humidEl = document.createElement('p')
-           humidEl.classList.add('card-text')
+                let windEl = document.createElement('p')
+                windEl.classList.add('card-text')
 
-           let tempEl = document.createElement('p')
-           tempEl.classList.add('card-text')
+                let humidEl = document.createElement('p')
+                humidEl.classList.add('card-text')
 
-           humidEl.textContent = `humidity : ${data.main.humidity} %`
+                let tempEl = document.createElement('p')
+                tempEl.classList.add('card-text')
 
-           tempEl.textContent = `temperature : ${data.main.temp}`
+                humidEl.textContent = `humidity : ${Math.round(data.main.humidity)} %`
 
-           windEl.textContent = `wind : ${data.wind.speed}`
+                tempEl.textContent = `temperature : ${Math.round(data.main.temp)}` 
+                
 
-           let imgEl = document.createElement('img')
-           imgEl.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+                windEl.textContent = `wind : ${Math.round(data.wind.speed)} mph`
 
-           titleEl.appendChild(imgEl)
-           cardBodyEl.appendChild(titleEl)
-           cardBodyEl.appendChild(tempEl)
-           cardBodyEl.appendChild(windEl)
-           cardBodyEl.appendChild(humidEl)
-           cardEl.appendChild(cardBodyEl)
-           todayEl.appendChild(cardEl)
-
-           getForecast(city)
-           getUVIndex(data.coord.lat, data.coord.lon)
-
-        //    let lat = city.coord.lat;
-        //    let lon = city.coord.lon;
-
-           function getUVIndex(lat) {
-
-           }
-
-           function getForecast(lon) {
-
-           }
-
-           //write func for lat and lon //2 sep api calls 
-        })
+                let imgEl = document.createElement('img')
+                imgEl.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
 
 
+                titleEl.appendChild(imgEl)
+                cardBodyEl.appendChild(titleEl)
+                cardBodyEl.appendChild(tempEl)
+                cardBodyEl.appendChild(windEl)
+                cardBodyEl.appendChild(humidEl)
+                cardEl.appendChild(cardBodyEl)
+                todayEl.appendChild(cardEl)
+            
+
+                function getUVIndex(lat, lon) {
+                    let UVQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+                    fetch(UVQueryURL)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            console.log(lat, lon)
+                            console.log(data.current.uvi)
+
+                            let uvEl = document.createElement('p')
+                            uvEl.classList.add('card-text')
+                            uvEl.textContent = `uv index : ${Math.round(data.current.uvi)}`
+
+                            cardBodyEl.appendChild(uvEl)
+
+                            function getuvColor() {
+
+                                if(uvEl <= 2){
+                                   uvEl.addClass = ("favorable")
+                               }
+                               
+                               else if(uvEl >2 && uvEl <=7){
+                                   uvEl.addClass = ("moderate")
+                               }
+                               
+                               else if(uvEl >8) {
+                                   uvEl.addClass = ("severe")
+                               };
+               
+                           }
+                           getuvColor()
+
+                        })
+                        
+                }
+
+                getUVIndex(data.coord.lat, data.coord.lon)
+
+
+            });
 
     }
 
-
 })
 
+
+
+//for 5 day forecast
+// let forecastQueryURL= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`
 
 
 
